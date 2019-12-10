c3eae1fead6f997afb5b410010712a828f17acb2
Ade Putra S <poetrajerami@gmail.com>
2019-12-10 16:05:26 +0700

    update control

--- CalendarPicker/Controls.js
+++ CalendarPicker/Controls.js
@@ -7,7 +7,7 @@ import PropTypes from 'prop-types';
 
 export default function Controls({ styles, textStyles, label, onPressControl, disabled, icon }) {
 
-  let content = icon ? icon : <Text style={[styles, textStyles]}>{label}</Text>;
+  let content = icon || <Text style={[styles, textStyles]}>{label}</Text>;
   return (
     <TouchableOpacity style={styles} onPress={() => onPressControl()}>
       {content}
